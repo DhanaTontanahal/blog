85773498446e159b723f1f73df7e9317472023ee
Dhana Shekhar Tontanahal <dhanashekhar.tontanahal@zemosolabs.com>
2020-09-04 16:17:53 +0530

    for deploying main app

---
+++ build/precache-manifest.326ce640c0caafbceff17fced1ff45d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c798f0eb096add043734f5963fa96f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "552aab7251802bd02c9d",
+    "url": "/static/js/2.b70cbd05.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b70cbd05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390983925b1a90ad1cf0",
+    "url": "/static/js/main.f453da06.chunk.js"
+  },
+  {
+    "revision": "23bd54350b6875d4f250",
+    "url": "/static/js/runtime-main.1c106980.js"
+  }
+]);
